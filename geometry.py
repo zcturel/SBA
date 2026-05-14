534ff01e75d569579a7b5713f894a2cf14aefbc6
cturel <cturel28@icloud.com>
2026-05-14 23:41:28 +0200

    Simulating a mathematical error for testing

--- geometry.py
+++ geometry.py
@@ -2,7 +2,7 @@ import math
 
 def triangle_area(base, height):
     """Calculates the area of a triangle."""
-    return 0.5 * base * height
+    return 0.6 * base * height
 
 def square_area(side):
     """Calculates the area of a square."""
